6c1ab63fdf6f9220e51544ed4ee19697d64711c3
Geowawa <49433465+Geowawa@users.noreply.github.com>
2019-12-05 11:23:13 +0100

    Add files via upload

---
+++ main.ps1
@@ -0,0 +1,39 @@
+﻿$film = "EnterFilmName"
+
+#get desktop path & create folder
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+New-Item -ItemType directory -Path $DesktopPath\$film
+
+#init download Object
+$WebClient = New-Object System.Net.WebClient
+
+# Open file dialog to get m3u file
+Add-Type -AssemblyName System.Windows.Forms
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+
+    InitialDirectory = [Environment]::GetFolderPath('Desktop')    
+}
+$null = $FileBrowser.ShowDialog()
+
+$n = 1000
+
+foreach($line in Get-Content $FileBrowser.FileName) {
+
+    if($line -match "http"){
+        Write-Host "Downloading File $n"
+        $WebClient.DownloadFile("$line","$DesktopPath\$film\$n.ts")
+        $n++
+    }
+}
+
+
+# create join.bat
+New-Item -Path $DesktopPath\$film\join.bat -ItemType File
+Add-Content -Path $DesktopPath\$film\join.bat "copy /b *.ts $film.ts"  
+Add-Content -Path $DesktopPath\$film\join.bat "move $film.ts $DesktopPath"  
+
+
+
+Write-Host "!!! FERTIG !!!"
+Write-Host "!!! FERTIG !!!"
